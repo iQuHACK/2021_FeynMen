a9615c5d407befc2bc0603c6acf87f4e62085307
CuauSuarez <a01206503@itesm.mx>
2021-01-31 09:05:21 -0600

    Add complete profiles

--- main.py
+++ main.py
@@ -17,31 +17,163 @@ pygame.init()
 
 # Profiles dictionary by list of properties
 profiles_dict = {
-    "[0,0,0,0]": {
+    "0000": {
         "Name": "Albert Einstein",
         "Age": 35,
         "Famous for": "Relativity",
-        "Bio": "Relatively attractive."
+        "Bio": "Relatively attractive.",
+        "Path": "profiles/einstein.jpg"
     },
-    "[0,0,0,1]": {
+    "0001": {
         "Name": "Marie Curie",
         "Age": 38,
         "Famous for": "Radiation",
-        "Bio": "A radiant beauty."
+        "Bio": "A radiant beauty.",
+        "Path": "profiles/curie.jpg"
     },
-    "[0,0,1,0]": {
+    "0010": {
         "Name": "David Deutsch",
         "Age": 47,
         "Famous for": "Father of Quantum Computing",
-        "Bio": "No discrete bits shall describe me."
+        "Bio": "No discrete bits shall describe me.",
+        "Path": "profiles/deutsch.jpg"
     },
-    "[0,0,1,1]": {
+    "0011": {
         "Name": "Euler",
         "Age": 28,
         "Famous for": "Everything Maths",
-        "Bio": "M is for Maths which belongs to me."
+        "Bio": "M is for Maths which belongs to me.",
+        "Path": "profiles/euler.jpg"
+    },
+    "0100": {
+        "Name": "Richard Feynman",
+        "Age": 40,
+        "Famous for": "Feynman Diagrams",
+        "Bio": "Every path leads to me.",
+        "Path": "profiles/feynman.jpg"
+    },
+    "0101": {
+        "Name": "Carl Friedrich Gauss",
+        "Age": 22,
+        "Famous for": "Maths and Electromagnetism.",
+        "Bio": "Baby, I'm magnetic.",
+        "Path": "profiles/gauss.jpg"
+    },
+    "0110": {
+        "Name": "Emmy Noether",
+        "Age": 26,
+        "Famous for": "Symmetry",
+        "Bio": "Perfect from whatever angle.",
+        "Path": "profiles/noether.jpg"
+    },
+    "0111": {
+        "Name": "Newton",
+        "Age": 23,
+        "Famous for": "Classical Gravity",
+        "Bio": "Classically attractive.",
+        "Path": "profiles/newton.jpg"
+    },
+    "1000": {
+        "Name": "Stephen Hawking",
+        "Age": 52,
+        "Famous for": "Hawking radiation.",
+        "Bio": "I no longer have good bios, please help!",
+        "Path": "profiles/hawking.jpg"
+    },
+    "1001": {
+        "Name": "Hedy Lamarr",
+        "Age": 30,
+        "Famous for": "Radar",
+        "Bio": "I got you on my sights.",
+        "Path": "profiles/lamarr.jpg"
+    },
+    "1010": {
+        "Name": "George Lemaitre",
+        "Age": 47,
+        "Famous for": "Big Bang Theory",
+        "Bio": "No, I didn't create that boring TV program.",
+        "Path": "profiles/lemaitre.jpg"
+    },
+    "1011": {
+        "Name": "Edwin Schrodinger",
+        "Age": 33,
+        "Famous for": "Wave-particle equation",
+        "Bio": "I love my cat.",
+        "Path": "profiles/schrodinger.jpg"
+    },
+    "1100": {
+        "Name": "Blaise Pascal",
+        "Age": 46,
+        "Famous for": "Probability and fluids",
+        "Bio": "I\'m under great pressure.",
+        "Path": "profiles/pascal.jpg"
+    },
+    "1101": {
+        "Name": "Peter Shor",
+        "Age": 53,
+        "Famous for": "Shor\'s algorithm",
+        "Bio": "Let me factorize you.",
+        "Path": "profiles/shor.jpg"
+    },
+    "1110": {
+        "Name": "Bernhard Riemann",
+        "Age": 55,
+        "Famous for": "Something something Riemann",
+        "Bio": "Zeta.",
+        "Path": "profiles/riemann.jpg"
+    },
+    "1111": {
+        "Name": "Ada Lovelace",
+        "Age": 21,
+        "Famous for": "First ever programmer.",
+        "Bio": "Program this.",
+        "Path": "profiles/lovelace.jpg"
     },
 }
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
+
+
 
 class Button(object):
     def __init__(self, text, color, x=None, y=None, **kwargs):
@@ -122,17 +254,21 @@ def starting_screen():
                 break
 
 
-def gate_choose():
+def gate_choose(question):
     screen.fill((0, 0, 0))
     screen.blit(bg, (0, 0))
     circuit.draw('mpl', filename='gifs/currentcircuit.png')
+    
+    game_title = font.render(question, True, WHITE)
+    screen.blit(game_title, (display_width // 2 - game_title.get_width() // 2, 20))
+    
     img = pygame.image.load('gifs/currentcircuit.png')
     img = pygame.transform.scale(img, (400, 200))
-    screen.blit(img, (50, 20))
+    screen.blit(img, (50, 80))
 
     img = pygame.image.load('currentmatrix.png')
     img = pygame.transform.scale(img, (500, 200))
-    screen.blit(img, (500, 20))
+    screen.blit(img, (500, 80))
 
     pygame.display.update()  # update the display
     game_title = font.render('Which gate would you like to choose?', True, RED)
@@ -323,13 +459,28 @@ def no_screen():
                 return 'exit'
                 break
 
-def match_screen():
+def match_screen(profile_att):
     screen.fill((0, 0, 0))
     screen.blit(bg, (0, 0))
     #circuit.draw('mpl', filename='gifs/currentcircuit.png')
-    img = pygame.image.load('profiles/einstein.jpg')
+    
+    profile_att = ''.join(profile_att)
+    
+    img = pygame.image.load(profiles_dict[profile_att]["Path"])
     img = pygame.transform.scale(img, (200, 300))
-    screen.blit(img, (display_width // 2 - 200 // 2, 20))
+    screen.blit(img, (display_width // 2 - 220, 20))
+    
+    name_s = font.render(profiles_dict[profile_att]["Name"], True, WHITE)
+    screen.blit(name_s, (display_width // 2, 20))
+    
+    age_s = font.render("Age: " + str(profiles_dict[profile_att]["Age"]), True, WHITE)
+    screen.blit(name_s, (display_width // 2, 120))
+    
+    famous_s = font.render("Famous for: " + profiles_dict[profile_att]["Famous for"], True, WHITE)
+    screen.blit(famous_s, (display_width // 2, 220))
+    
+    bio_s = font.render(profiles_dict[profile_att]["Bio"], True, WHITE)
+    screen.blit(bio_s, (display_width // 2, 320))
 
     pygame.display.update()  # update the display
     game_title = font.render('It\'s a match', True, WHITE)
@@ -397,9 +548,18 @@ while True:
     time.sleep(0.5)
     if playornot:
         choice = []
-
+        questions_pool = [0,0,0,0]
+        
+        questions_pool[0] = "Is your scientist male?"
+        questions_pool[1] = "Does your scientist know Maths?"
+        questions_pool[2] = "Too hot to handle?"
+        questions_pool[3] = "Too cold to hold?"
+        
+        profile_att = []
+        flag = 0
+        
         while True:
-            gate_chosen = gate_choose()
+            gate_chosen = gate_choose(questions_pool[flag])
             time.sleep(0.5)
             if gate_chosen == 'C01':
                 choice.append(gate_chosen)
@@ -419,17 +579,32 @@ while True:
             if gate_chosen == 'DONE':
                 value = comparison(choice, matriz, coeff)
                 if value:
-                    print('Go to yes screen')
-                    continueornot = match_screen()
-                    time.sleep(0.5)
-                    break
+                    profile_att.append("1")
+                    flag += 1
+                    #continueornot = match_screen()
+                    #time.sleep(0.5)
+                    #break
+                    #continue
                 else:
-                    print('Go to no screen')
-                    continueornot = match_screen()
-                    time.sleep(0.5)
+                    profile_att.append("0")
+                    flag += 1
+                    #continueornot = match_screen()
+                    #time.sleep(0.5)
+                    #break
+                    #continue
+                if flag == 4:
                     break
             time.sleep(0.5)
+            
+        while True:
+            time.sleep(0.5)
+            print(profile_att)
+            continueornot = match_screen(profile_att)
+            time.sleep(0.5)
+            break
+            
         if continueornot == 'exit':
             break
+            
     else:
         break
